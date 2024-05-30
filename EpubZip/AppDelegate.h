92888ee545af0fd309afbb2d57d0c84e2918e34c
Danil Korotenko <danil.korotenko@gmail.com>
2024-05-30 12:47:18 +0300

    Initial Commit

---
+++ EpubZip/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  EpubZip
+//
+//  Created by Danil Korotenko on 5/30/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
